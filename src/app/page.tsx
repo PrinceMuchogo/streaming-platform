c16bea1063c21211b3f4a01f9ed4dcde3879c873
Prince Muchogo <108458278+PrinceMuchogo@users.noreply.github.com>
2025-04-12 18:29:44 +0200

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,16 @@
-import ECommerce from "@/components/Dashboard/E-commerce";
 import { Metadata } from "next";
-import DefaultLayout from "@/components/Layouts/DefaultLaout";
 import React from "react";
 
 export const metadata: Metadata = {
   title:
-    "Next.js E-commerce Dashboard Page | NextAdmin - Next.js Dashboard Kit",
-  description: "This is Next.js Home page for NextAdmin Dashboard Kit",
+    "Streaming Platform",
+  description: "Streaming Platform",
 };
 
 export default function Home() {
   return (
-    <>
-      <DefaultLayout>
-        <ECommerce />
-      </DefaultLayout>
-    </>
+    <div>
+      Home Page
+    </div>
   );
 }
